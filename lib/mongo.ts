cdddd4cb3c39bc190be744a32c7c856185e9abbb
Brice Duke <bricewduke@gmail.com>
2022-12-07 19:21:20 -0600

    fix(mongo): fix db connection error messages

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -3,8 +3,12 @@ import { Db, MongoClient } from 'mongodb';
 const MONGODB_URI = process.env.MONGODB_URI || "";
 const DB_NAME = process.env.DB_NAME || "";
 
-if (!MONGODB_URI || !DB_NAME) {
-	throw new Error("Add db vars to .env");
+if (!MONGODB_URI) {
+	throw new Error("Add db uri to .env");
+}
+
+if (!DB_NAME) {
+	throw new Error("Add db name to .env");
 }
 
 let cachedClient: MongoClient;
